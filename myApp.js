38e8a217c8d6e1ba38727a5c1a647c957eeb3bf3
helminolimit <58017701+helminolimit@users.noreply.github.com>
2022-07-20 16:06:30 +0800

    a1

--- myApp.js
+++ myApp.js
@@ -4,9 +4,10 @@ let app = express();
 console.log("Hello World");
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html");
-    app.use("/public", express.static(__dirname + "/public"));
+    res.sendFile(__dirname + "/views/index.html");   
 });
+
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
     process.env.MESSAGE_STYLE === "uppercase" ?
