64c2bcaa906061ee2db9963b9be0aebd811d0109
helminolimit <58017701+helminolimit@users.noreply.github.com>
2022-07-20 12:18:44 +0800

    d

--- myApp.js
+++ myApp.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
-    if (process.env.MESSAGE_STYLE === "uppercase") {
+    if (process.env.MESSAGE_STYLE = "uppercase") {
         response = "Hello JSON".toUpperCase();
       } else {
         response = "Hello json";
