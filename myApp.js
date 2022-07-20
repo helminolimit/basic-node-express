f841edf062986ca578b8390fa510c54d8a786f95
helminolimit <58017701+helminolimit@users.noreply.github.com>
2022-07-20 16:03:30 +0800

    k

--- myApp.js
+++ myApp.js
@@ -1,12 +1,12 @@
-require('dotenv').config();
+require('.env').config();
 let express = require('express');
 let app = express();
 console.log("Hello World");
 
-// app.get("/", (req, res) => {
-//     res.sendFile(__dirname + "/views/index.html");
-//     app.use("/public", express.static(__dirname + "/public"));
-// });
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+    app.use("/public", express.static(__dirname + "/public"));
+});
 
 app.get("/json", (req, res) => {
     process.env.MESSAGE_STYLE === "uppercase" ?
